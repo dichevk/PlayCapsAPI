859e2bce9417b02bc79587a5a5acef9e85fad602
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-23 22:57:43 +0200

    add review and reviewer models

---
+++ ReviewApp/ReviewApp/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayCapsViewer.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ ReviewApp/ReviewApp/Models/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayCapsViewer.Models
+{
+    public class Reviewer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }   
+    }
+}
