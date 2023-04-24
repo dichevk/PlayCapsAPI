75b2cb4e6fa679e76c567f5b2329b29e7fb7a998
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-24 23:19:19 +0200

    review and reviewer relationships

--- ReviewApp/ReviewApp/Models/Review.cs
+++ ReviewApp/ReviewApp/Models/Review.cs
@@ -8,5 +8,10 @@ namespace PlayCapsViewer.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+
+        public int Rating { get; set; }
+
+        public Reviewer Reviewer { get; set; }
+        public PlayCap PlayCap { get; set; }
     }
 }
--- ReviewApp/ReviewApp/Models/Reviewer.cs
+++ ReviewApp/ReviewApp/Models/Reviewer.cs
@@ -8,5 +8,6 @@ namespace PlayCapsViewer.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }   
+        public ICollection <Review> Reviews { get; set; }
     }
 }
