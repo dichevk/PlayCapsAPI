557c15b59fa47afbad351842b670d0821d0a4a14
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-28 23:29:47 +0200

    adding dbseed and app connection

---
+++ ReviewApp/ReviewApp/DbSeed.cs
@@ -0,0 +1,120 @@
+﻿using PlayCapsViewer.Data;
+using PlayCapsViewer.Models;
+
+namespace PlayCapsViewer
+{
+    public class DbSeed
+    {
+        private readonly DataContext dataContext;
+        public DbSeed(DataContext context)
+        {
+            this.dataContext = context;
+        }
+        public void SeedDataContext()
+        {
+            if (!dataContext.PlayCapsPlayers.Any())
+            {
+                var pokemonOwners = new List<PlayCapsPlayer>()
+                {
+                    new PlayCapsPlayer()
+                    {
+                        PlayCap = new PlayCap()
+                        {
+                            Name = "Pikachu",
+                            StartDate = new DateTime(1903,1,1),
+                            EndDate = new DateTime(1905,1,1),
+                            PlayCapsCategory= new List<PlayCapsCategory>()
+                            {
+                                new PlayCapsCategory { Category = new Category() { Name = "Electric"}}
+                            },
+                            Reviews = new List<Review>()
+                            {
+                                new Review { Title="Pikachu",Text = "Pickahu is the best pokemon, because it is electric", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Teddy", LastName = "Smith" } },
+                                new Review { Title="Pikachu", Text = "Pickachu is the best a killing rocks", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Taylor", LastName = "Jones" } },
+                                new Review { Title="Pikachu",Text = "Pickchu, pickachu, pikachu", Rating = 1,
+                                Reviewer = new Reviewer(){ FirstName = "Jessica", LastName = "McGregor" } },
+                            }
+                        },
+                        Player = new Player()
+                        {
+                            FirstName = "Jack",
+                            LastName = "London",
+                            Gym = "Brocks Gym",
+                            Country = new Country()
+                            {
+                                Name = "Kanto"
+                            }
+                        }
+                    },
+                    new PlayCapsPlayer()
+                    {
+                        PlayCap = new PlayCap()
+                        {
+                            Name = "Squirtle",
+                            StartDate = new DateTime(1903,1,1),
+                            PlayCapsCategory = new List<PlayCapsCategory>()
+                            {
+                                new PlayCapsCategory { Category = new Category() { Name = "Water"}}
+                            },
+                            Reviews = new List<Review>()
+                            {
+                                new Review { Title= "Squirtle", Text = "squirtle is the best pokemon, because it is electric", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Teddy", LastName = "Smith" } },
+                                new Review { Title= "Squirtle",Text = "Squirtle is the best a killing rocks", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Taylor", LastName = "Jones" } },
+                                new Review { Title= "Squirtle", Text = "squirtle, squirtle, squirtle", Rating = 1,
+                                Reviewer = new Reviewer(){ FirstName = "Jessica", LastName = "McGregor" } },
+                            }
+                        },
+                        Player = new Player()
+                        {
+                            FirstName = "Harry",
+                            LastName = "Potter",
+                            Gym = "Mistys Gym",
+                            Country = new Country()
+                            {
+                                Name = "Saffron City"
+                            }
+                        }
+                    },
+                     new PlayCapsPlayer()
+                    {
+                        PlayCap = new PlayCap()
+                        {
+                            Name = "Venasuar",
+                            StartDate = new DateTime(1903,1,1),
+                            EndDate = new DateTime(1906,1,1),
+                            PlayCapsCategory = new List<PlayCapsCategory>()
+                            {
+                                new PlayCapsCategory { Category = new Category() { Name = "Leaf"}}
+                            },
+                            Reviews = new List<Review>()
+                            {
+                                new Review { Title="Veasaur",Text = "Venasuar is the best pokemon, because it is electric", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Teddy", LastName = "Smith" } },
+                                new Review { Title="Veasaur",Text = "Venasuar is the best a killing rocks", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Taylor", LastName = "Jones" } },
+                                new Review { Title="Veasaur",Text = "Venasuar, Venasuar, Venasuar", Rating = 1,
+                                Reviewer = new Reviewer(){ FirstName = "Jessica", LastName = "McGregor" } },
+                            }
+                        },
+                        Player = new Player()
+                        {
+                            FirstName = "Ash",
+                            LastName = "Ketchum",
+                            Gym = "Ashs Gym",
+                            Country = new Country()
+                            {
+                                Name = "Millet Town"
+                            }
+                        }
+                    }
+                };
+                dataContext.PlayCapsPlayers.AddRange(pokemonOwners);
+                dataContext.SaveChanges();
+            }
+        }
+    }
+}
--- ReviewApp/ReviewApp/Program.cs
+++ ReviewApp/ReviewApp/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PlayCapsViewer;
+using PlayCapsViewer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//add the db seed 
+builder.Services.AddTransient<DbSeed>();
+
+//bring over the db specified in the DataContext.cs file
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    // connect to the db via the connection string 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}); 
 
 var app = builder.Build();
+
+//seed app connection
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<DbSeed>();
+        service.SeedDataContext();
+    }
+}
 
 // Configure the HTTP request pipeline. Middleware section that includes authorization, https redirection and mapping 
 if (app.Environment.IsDevelopment())
