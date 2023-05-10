6aab840ec7332befed6d5b1a00517ddf76faf134
KonstantinDichev <kdichev.nl@gmail.com>
2023-05-10 12:18:18 +0200

    implement bidirectional mapping profiles

--- ReviewApp/ReviewApp/Helper/MappingProfiles.cs
+++ ReviewApp/ReviewApp/Helper/MappingProfiles.cs
@@ -1,6 +1,27 @@
-﻿namespace PlayCapsViewer.Helper
+﻿using PlayCapsViewer.Models;
+using AutoMapper;
+using PlayCapsViewer.DTO;
+using System;
+
+namespace PlayCapsViewer.Helper
 {
-    public class MappingProfiles
+    public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+            CreateMap<Category, CategoryDTO>();
+            CreateMap<PlayCap, PlayCapDTO>();
+            CreateMap<Player, PlayerDTO>();
+            CreateMap<Country, CountryDTO>();
+            CreateMap<Reviewer, ReviewerDTO>();
+            CreateMap<Review, ReviewDTO>();
+
+            CreateMap<CategoryDTO, Category>();
+            CreateMap<PlayCapDTO, PlayCap>();
+            CreateMap<PlayerDTO, Player>();
+            CreateMap<CountryDTO, Country>();
+            CreateMap<ReviewerDTO, Reviewer>();
+            CreateMap<ReviewDTO, Review>();
+        }
     }
 }
